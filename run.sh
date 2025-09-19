26bd96e9d7cd33d6136d4dcd332be24114c4e6ef
GoodNormal <35917641+GoodNormal@users.noreply.github.com>
2025-09-20 00:05:39 +0800

    fix: 更新服务器启动脚本为paper并调整内存配置
    将Velocity服务器启动脚本更新为paper服务器，同时将JVM内存参数从512m调整为10g以提升性能

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ fi
 echo "代码库同步完成"
 
 # 启动Velocity代理服务器
-echo "正在启动Velocity服务器..."
-"/data/program/java/java21/bin/java" -Xms512m -Xmx512m -javaagent:authlib-injector.jar=https://skin.mualliance.ltd/api/union/yggdrasil -Dfile.encoding=UTF-8 -Duser.language=zh -Duser.country=CN -jar server.jar
+echo "正在启动paper服务器..."
+"/data/program/java/java21/bin/java" -Xms10g -Xmx10g -javaagent:authlib-injector.jar=https://skin.mualliance.ltd/api/union/yggdrasil -Dfile.encoding=UTF-8 -Duser.language=zh -Duser.country=CN -jar server.jar
 
 echo "服务器已停止"
